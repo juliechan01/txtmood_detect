bf8ca54e2543e93fb7d64f7e868cd3136636949d
Julie Chan <juliechan03@gmail.com>
2024-06-08 02:04:47 -0700

    vercel mods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import EditText from './views/EditText';
 import './App.css';
-// import '../tailwind.config';
 
 function App() {
+  const [ theme, setTheme ] = useState('light');
+  const toggleTheme = () => {
+    if(theme === 'light') {
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }
 
   return (
-    <div className='bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-800 h-screen'>
+    <div className={` App ${theme} bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-800 h-screen`}>
       <BrowserRouter>
+      <div className='rounded-full'>
+        <button onClick={toggleTheme}>🌞 || 🌚</button>
+      </div>
         <Routes>
           <Route element={<Main />} path='/' default/>
           <Route element={<EditText />} path='/texts/:id' />
